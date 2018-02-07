282c62cfa52caae2a975ce3b85c976f98dfa8159
Swapnil Jain <swapnil@mask365.com>
2018-02-07 15:02:18 +0530

    chapter3 scripts

---
+++ Chapter3/rbacdevuser1.sh
@@ -0,0 +1,9 @@
+openssl genrsa -out devuser1.key 2048
+openssl req -new -key devuser1.key -out devuser1.csr -subj "/CN=devuser1/O=dev"
+openssl x509 -req -in devuser1.csr \
+  -CA /etc/kubernetes/pki/ca.crt \
+  -CAkey /etc/kubernetes/pki/ca.key \
+  -CAcreateserial -out devuser1.crt -days 45
+
+
+
