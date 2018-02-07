f34428a453f2b75598a5f8569064d0d93610dc29
Swapnil Jain <swapnil@mask365.com>
2018-02-07 13:03:22 +0530

    chapter3 scripts

--- Chapter3/api.sh
+++ Chapter3/api.sh
@@ -3,4 +3,5 @@ APISERVER=$(kubectl config view | grep server | cut -f 2- -d ":" | tr -d " ")
 echo Getting Auth Token
 TOKEN=$(kubectl describe secret $(kubectl get secrets | grep default | cut -f1 -d ' ') | grep -E '^token' | cut -f2 -d':' | tr -d '\t')
 echo Access API using curl
+echo curl $APISERVER/api --header "Authorization: Bearer $TOKEN" --insecure
 curl $APISERVER/api --header "Authorization: Bearer $TOKEN" --insecure
