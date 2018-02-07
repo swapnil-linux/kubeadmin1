2b4938b013f464ad4320d7e06676e07ea4972f1b
Swapnil Jain <swapnil@mask365.com>
2018-02-07 13:00:31 +0530

    chapter3 scripts

--- Chapter3/api.sh
+++ Chapter3/api.sh
@@ -1,15 +1,6 @@
+echo Getting APISERVER address and port
 APISERVER=$(kubectl config view | grep server | cut -f 2- -d ":" | tr -d " ")
+echo Getting Auth Token
 TOKEN=$(kubectl describe secret $(kubectl get secrets | grep default | cut -f1 -d ' ') | grep -E '^token' | cut -f2 -d':' | tr -d '\t')
+echo Access API using curl
 curl $APISERVER/api --header "Authorization: Bearer $TOKEN" --insecure
-{
-  "kind": "APIVersions",
-  "versions": [
-    "v1"
-  ],
-  "serverAddressByClientCIDRs": [
-    {
-      "clientCIDR": "0.0.0.0/0",
-      "serverAddress": "10.0.1.149:443"
-    }
-  ]
-}
