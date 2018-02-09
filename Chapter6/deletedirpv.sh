70b195a80b0e98ce0a82c46c8cb11cf3b7369c60
Swapnil Jain <swapnil@mask365.com>
2018-02-09 16:18:54 +0530

    published on amazon

--- Chapter6/deletedirpv.sh
+++ Chapter6/deletedirpv.sh
@@ -3,8 +3,8 @@ kubectl delete pv pv10gb
 kubectl delete pv pv20gb
 for node in `kubectl get nodes|grep -v NAME|awk '{print $1}'`
  do 
-   echo creating /mnt/data1 on $node
+   echo deleting /mnt/data1 on $node
    ssh swapnil@$node sudo rm -rf /mnt/data1
-   echo creating /mnt/data2 on $node
+   echo deleting /mnt/data2 on $node
    ssh swapnil@$node sudo rm -rf /mnt/data2
 done
