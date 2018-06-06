7d8bdc0ef49ce7e6d44a86705b73379e8382a24d
Swapnil Jain <swapnil@mask365.com>
2018-06-06 15:02:48 +0530

    fixed cluster ip in mysql-deployment

--- Chapter7/deletedirpv.sh
+++ Chapter7/deletedirpv.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 kubectl delete pv pv10gb
 kubectl delete pv pv20gb
-for node in `kubectl get nodes|grep -v NAME|awk '{print $1}'`
- do 
-   echo deleting /mnt/data1 on $node
-   ssh swapnil@$node sudo rm -rf /mnt/data1
-   echo deleting /mnt/data2 on $node
-   ssh swapnil@$node sudo rm -rf /mnt/data2
-done
